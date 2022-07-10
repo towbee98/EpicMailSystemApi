ff76441f7ebb2131f371f2c4b020c2e56973d52b
Oladele Tobiloba <tobiemma200@gmail.com>
2022-07-10 15:49:51 +0100

    changed the package.json

--- dist/config/env.js
+++ dist/config/env.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-//console.log(process.env.MONGO_URI)
+// console.log( process.env.MONGO_URI)
 const config = {
     MONGO_URI: process.env.MONGO_URI,
     PORT: process.env.PORT,
