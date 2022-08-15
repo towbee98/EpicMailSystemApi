3afe951236ef76b7b3e281487d1662497f3b5b7c
Tobiloba <35341202+towbee98@users.noreply.github.com>
2022-08-15 17:00:32 +0100

    Deleted the dist folder

--- dist/docs/auth/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// import signUp fro../messages/signUpnUp"
-// import login fro../messages/logingin"
-// export default{
-//     paths:{
-//         '/api/v1/auth/signup':{...signUp},
-//         '/api/v1/auth/login':{...login}
-//     }
-// }
--- dist/docs/users/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// import updatePassword from "../messages/updatePassword"
-// export default{
-//     paths:{
-//          '/api/v1/users/updatePassword':{...updatePassword}
-//     }
-// }
