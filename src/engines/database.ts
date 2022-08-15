fb6d2cdf7e532aca7b7ffde4e0ce67285f8e6039
Oladele Tobiloba <tobiemma200@gmail.com>
2022-08-15 16:31:00 +0100

    fix:added  gitignore file to the dist folder

--- src/engines/database.ts
+++ src/engines/database.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 import config from '../config/env';
-//console.log(config.MONGO_URI)
+
+// eslint-disable-next-line @typescript-eslint/ban-types
 const ConnectToDB = async (cb: Function) => {
+  // eslint-disable-next-line no-useless-catch
   try {
     await mongoose.connect(`${config.MONGO_URI}`);
     console.log('Database connected succesfully');
